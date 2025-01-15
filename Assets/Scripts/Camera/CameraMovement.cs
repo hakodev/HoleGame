e287f14018cad9e1fce10a03831bb4f5829edc36
MishotoDraganov <mdraganov04@gmail.com>
2025-01-15 06:17:42 +0100

    bigmergescenechanges

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -1,3 +1,112 @@
+
+using UnityEngine;
+public class CameraMovement : MonoBehaviour
+{
+    [Header("Mouse Input")]
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private bool invertCameraXAxis;
+    [SerializeField] private bool invertCameraYAxis;
+    private float verticalRotation;
+
+    [Header("Crouching")]
+    [SerializeField] private float uncrouchedCameraHeight;
+    [SerializeField] private float crouchedCameraHeight;
+    [SerializeField] private float cameraHeightSwitchSpeed;
+
+    private PlayerController playerController;
+    private Alteruna.Avatar avatar;
+
+    private float mouseX, mouseY;
+
+    private void Awake()
+    {
+        avatar = GetComponentInParent<Alteruna.Avatar>();
+        playerController = GetComponentInParent<PlayerController>();
+    }
+
+    private void Start()
+    {
+
+        if (!avatar.IsMe)
+        {
+            return;
+        }
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        // if (!IsOwner) return;
+        if (!avatar.IsMe)
+        {
+            return;
+        }
+        ProcessCameraInput();
+        ProcessCamera();
+    }
+
+    private void LateUpdate()
+    {
+        //  if (!IsOwner) return;
+
+        if (!avatar.IsMe)
+        {
+            return;
+        }
+        ProcessCameraCrouched();
+    }
+
+    private void ProcessCameraInput()
+    {
+        //Horizontal rotation
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        if (invertCameraXAxis)
+            mouseX = -mouseX;
+
+        //Vertical rotation
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        if (invertCameraYAxis)
+            mouseY = -mouseY;
+    }
+
+    private void ProcessCamera()
+    {
+        if (!playerController.MovementEnabled)
+        {
+            ResetCameraValues();
+            return;
+        }
+
+        playerController.transform.Rotate(Vector3.up, mouseX);
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+
+        this.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+    }
+
+    private void ProcessCameraCrouched()
+    {
+        Vector3 uncrouchedCamera = new(this.transform.localPosition.x, uncrouchedCameraHeight, this.transform.localPosition.z);
+        Vector3 crouchedCamera = new(this.transform.localPosition.x, crouchedCameraHeight, this.transform.localPosition.z);
+
+        this.transform.localPosition = playerController.IsCrouching
+            ? Vector3.Lerp(this.transform.localPosition, crouchedCamera, cameraHeightSwitchSpeed * Time.fixedDeltaTime)
+            : Vector3.Lerp(this.transform.localPosition, uncrouchedCamera, cameraHeightSwitchSpeed * Time.fixedDeltaTime);
+    }
+
+    private void ResetCameraValues()
+    {
+        Vector3 resetToUncrouchedCamera = new(this.transform.localPosition.x, uncrouchedCameraHeight, this.transform.localPosition.z);
+        this.transform.localPosition = resetToUncrouchedCamera;
+    }
+}
+
+
+
+/*
+
 using UnityEngine;
 using Alteruna;
 using DG.Tweening;
@@ -117,7 +226,7 @@ public class CameraMovement : MonoBehaviour {
 
             // playerHead.transform.Rotate(verticalRotation, horizontalRotation, 0);
         }
-        */
+        
     }
     private void ProcessCameraInput()
     {
@@ -162,3 +271,4 @@ public class CameraMovement : MonoBehaviour {
         this.transform.localPosition = resetToUncrouchedCamera;
     }
 }
+*/
