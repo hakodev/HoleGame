a1b2ab60f3c6984ff901a4dbecb6e4c60d061823
Ivan Lazarov <vankata1003@gmail.com>
2025-01-17 15:59:08 +0100

    zashto be

--- Assets/Scripts/DrawingStuff/MousePainter.cs
+++ Assets/Scripts/DrawingStuff/MousePainter.cs
@@ -24,24 +24,31 @@ public class MousePainter : MonoBehaviour{
 		paintManager = FindAnyObjectByType<PaintManager>();
 	}
 
-	public void Paint()
+	public void Update()
 	{
-        Vector3 position = Input.mousePosition;
-        Ray ray = cam.ScreenPointToRay(position);
-        RaycastHit hit;
+        bool click;
+        click = mouseSingleClick ? Input.GetMouseButtonDown(0) : Input.GetMouseButton(0);
 
-        if (Physics.Raycast(ray, out hit, range, notPlayerMask))
+        if (click)
         {
-            Paintable p = hit.collider.GetComponent<Paintable>();
-            if (p != null)
+            Vector3 position = Input.mousePosition;
+            Ray ray = cam.ScreenPointToRay(position);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100.0f, notPlayerMask))
             {
-                PaintManager.Instance.paint(p, hit.point, radius, hardness, strength, paintColor);
+                Debug.DrawRay(ray.origin, hit.point - ray.origin, Color.red);
+                Paintable p = hit.collider.GetComponent<Paintable>();
+                if (p != null)
+                {
+                    PaintManager.Instance.paint(p, hit.point, radius, hardness, strength, paintColor);
+                }
             }
         }
 
 
 
-
     }
+
 
 }
