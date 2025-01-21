80fb52abece7615806613e2ba6fcb28b58cc2a5e
Ivan Lazarov <vankata1003@gmail.com>
2025-01-21 15:41:00 +0100

    sga

--- Assets/Scripts/Symptoms/DespawningItems.cs
+++ Assets/Scripts/Symptoms/DespawningItems.cs
@@ -1,10 +1,19 @@
 using DG.Tweening;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DespawningItems : MonoBehaviour {
+
     public static void DespawnItem(GameObject heldObject) {
         heldObject.GetComponent<Collider>().enabled = false;
-        heldObject.transform.DOScale(new Vector3(0f, 0f, 0f), 1f);
+        Renderer[] renderers = heldObject.GetComponentsInChildren<Renderer>();
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.material.shader = Shader.Find("Shader Graphs/Glitch");
+            renderer.material.DOFloat(4, "_GlitchStrength", 0.4f);
+        }
+        heldObject.transform.DOScale(new Vector3(0f, 0f, 0f), 0.4f);
         Debug.Log("Despawn symptom triggered!");
     }
 }
