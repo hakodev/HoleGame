407c1079d401ee215be028ddac6da79c461bfb91
Ahmet Hakan Koç <ahmetkoc.business@gmail.com>
2025-01-23 19:33:23 +0100

    Carpet symptom finished

--- Assets/Scripts/Symptoms/CarpetData.cs
+++ Assets/Scripts/Symptoms/CarpetData.cs
@@ -1,12 +1,26 @@
+using Alteruna;
 using UnityEngine;
 
-public class CarpetData : MonoBehaviour {
+public class CarpetData : AttributesSync {
     [SerializeField] private CarpetColor carpetColor;
 
-    [field: SerializeField]
-    public bool IsCorrupted { get; set; }
+    [SerializeField, SynchronizableField]
+    private bool isCorrupted;
+
+    public bool IsCorrupted {
+        get { return isCorrupted; }
+        set { isCorrupted = value; }
+    }
 
     public CarpetColor GetColor() {
         return carpetColor;
+    }
+
+    private void OnCollisionEnter(Collision otherCollider) {
+        if(this.isCorrupted && otherCollider.collider.CompareTag("Player")) {
+            if(otherCollider.collider.GetComponent<PlayerRole>().GetRole() == Roles.Machine) {
+                otherCollider.collider.gameObject.GetComponent<PlayerController>().Jump();
+            }
+        }
     }
 }
