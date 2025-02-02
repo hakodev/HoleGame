73d092ee141a673ffe644a32121e63d78d1946d6
Ivan Lazarov <vankata1003@gmail.com>
2025-02-02 10:42:11 +0100

    cici

--- Assets/Scripts/LobbySystem.cs
+++ Assets/Scripts/LobbySystem.cs
@@ -31,7 +31,7 @@ public class LobbySystem : MonoBehaviour {
 
         // we are calling the symptom twice with these, once in setsymptom and once in the chain of calls bc we are displaying the notif canvas
 
-        /*
+        
         if(Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1)) {
             SymptomsManager.Instance.JustSetSymptom(0); // Inverted controls
             ResetCarpetParams();
@@ -58,7 +58,7 @@ public class LobbySystem : MonoBehaviour {
             ResetCarpetParams();
             DisplayNoSymptomText();
         }
-        */
+        
     }
 
     private void OnDestroy() {
