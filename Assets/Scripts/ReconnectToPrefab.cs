11c6019ab1bee534db186071fdbc36ce51eb52d4
Ahmet Hakan Koç <ahmetkoc.business@gmail.com>
2025-02-04 16:47:06 +0100

    Fixed sync values and colliders

--- Assets/Scripts/ReconnectToPrefab.cs
+++ Assets/Scripts/ReconnectToPrefab.cs
@@ -2,8 +2,26 @@ using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor.SceneManagement;
 
-public class ReconnectPrefab : MonoBehaviour
-{
-   
+public class ReconnectPrefab : MonoBehaviour {
+    [MenuItem("Assets/Reconnect Prefabs With Tag replacingThisObjectWithPrefab")]
+    private static void Reconnect() {
+        //string prefabPath = "Assets/Meshes/Prefab/SM_sink.prefab";
+        //GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+        Vector3 storedScale = Vector3.zero;
+        List<GameObject> foundObjects = GameObject.FindGameObjectsWithTag("replacingThisObjectWithPrefab").ToList();
+        for(int i = 0; i < foundObjects.Count; i++) {
+            if(PrefabUtility.GetPrefabAssetType(foundObjects[i]) == PrefabAssetType.NotAPrefab) { continue; }
+            //GameObject prefab = PrefabUtility.GetCorrespondingObjectFromSource(foundObjects[i]);
+            if(PrefabUtility.GetPrefabInstanceStatus(foundObjects[i]) == PrefabInstanceStatus.Connected) {
+                storedScale = foundObjects[i].transform.localScale;
+                PrefabUtility.RevertPrefabInstance(foundObjects[i], InteractionMode.UserAction);
+                foundObjects[i].transform.localScale = storedScale;
+                //EditorUtility.SetDirty(obj);
+            }
+            Debug.Log(PrefabUtility.GetPrefabInstanceStatus(foundObjects[i]) + " " + foundObjects[i].name);
+        }
+        EditorSceneManager.MarkSceneDirty(UnityEngine.SceneManagement.SceneManager.GetActiveScene());
+    }
 }
